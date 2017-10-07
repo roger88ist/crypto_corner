cdbbee374bb7d7c28394b4c217478a1908ff5c8a
Rogerio I <roger88ist@gmail.com>
2017-10-07 10:46:49 -0400

    #26 - add dependent: destroy to trades for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :trades
+  has_many :trades, dependent: destroy
 end
