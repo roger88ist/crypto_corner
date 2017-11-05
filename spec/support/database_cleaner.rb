9bb4bec7336f0be69d536448976b713c57e0c8d2
Rogerio I <roger88ist@gmail.com>
2017-11-05 13:47:57 -0500

    added gem 'database_cleaner'

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,24 @@
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # this may be needed for Capybara tests and for testing after_commit hooks
+  config.before(:each, strategy: :truncation) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
