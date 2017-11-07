ec354b52fbbf5510dead4de669bc5ed8f439e6e6
Rogerio I <roger88ist@gmail.com>
2017-11-07 18:37:22 -0500

    create purchase scoop for Trade

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,6 +3,7 @@ class Trade < ApplicationRecord
 
   scope :by_user, -> (user) { where(user_id: user.id) }
   scope :order_by_date, -> { order(:date) }
+  scope :purchases, -> { where(trade_type: 'buy') }
 
   def self.for_trades_index(current_user)
   	by_user(current_user).order_by_date
