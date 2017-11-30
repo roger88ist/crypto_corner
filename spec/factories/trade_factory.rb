af3a135b06d255e18f876671003b13fb3c722460
Rogerio I <roger88ist@gmail.com>
2017-11-29 19:59:59 -0500

    create :sell trait for Trade factory

--- spec/factories/trade_factory.rb
+++ spec/factories/trade_factory.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :trade do
 
-  	symbol      { 'btc' }
+    symbol      { 'btc' }
     total_coins { 10 }
     dollars     { 100 }
     price       { 10 }
 
     trait :buy do
       trade_type  { 'buy' }
+    end
+
+    trait :sell do
+      trade_type  { 'sell' }
     end
   end
 end
